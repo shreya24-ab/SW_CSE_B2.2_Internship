c56426c30a78f1c1a0f7383b9ff2da5382841715
shreya-24ab <shreyabyali07@gmail.com>
2026-08-28 17:06:39 +0530

    add

--- Day/Day13/Day-13-react-app/src/App.jsx
+++ Day/Day13/Day-13-react-app/src/App.jsx
@@ -1,17 +1,24 @@
 import { Welcome } from "./components/P1";
 import { ProductCard } from "./components/P2";
-import './App.css'
+import headphones from "./assets/sony.jpg";
+import  ButtonDemo from "./components/P3";
+import './App.css';
+
 function App() {
 
   return (
     <>
-      <Welcome name="Rakesh"/>
-      <ProductCard 
-      name="Sony ULT Wear Noise Cancellation Wireless Bluetooth Over Ear Headphones with Massive Bas"
-      price={15999}
-      image="https://m.media-amazon.com/images/I/610DB8Cwm7L._SY450_.jpg" 
+      <Welcome name="shreya"/>
+            <ButtonDemo/>
 
-    />
+      <ProductCard
+      // name="Sony ULT Wear Noise Cancellation Wireless Bluetooth Over Ear Headphones with Massive"
+      price={15999}
+      // image="https://m.media-amazon.com/images/I/610DB8Cwm7L._SY450_.jpg"
+      image={headphones}
+      />
+      {/* comments */}
+      
     </>
   )
 }
